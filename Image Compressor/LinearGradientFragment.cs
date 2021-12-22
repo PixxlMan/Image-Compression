c59e85d4e25a494f6b5d4f06b92edfcc43a35ff3
Pixxl <oliverhalenius@yahoo.se>
2021-12-22 01:18:48 +0100

    implement writing, reading and drawing with optimal directions

--- Image Compressor/LinearGradientFragment.cs
+++ Image Compressor/LinearGradientFragment.cs
@@ -8,19 +8,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Image_Compressor.ColorUtils;
 
 namespace Image_Compressor
 {
 	public struct LinearGradientFragment : Fragment
 	{
-		public LinearGradientFragment(Rgb24 aColor, Rgb24 bColor)
+		public LinearGradientFragment(Rgb24 aColor, Rgb24 bColor, SampleLine sampleLine)
 		{
 			this.aColor = aColor;
 			this.bColor = bColor;
+			this.sampleLine = sampleLine;
 		}
 
 		private Rgb24 aColor;
 		private Rgb24 bColor;
+		private SampleLine sampleLine;
 
 		public FragmentId Id => ConstId;
 
@@ -28,7 +31,33 @@ namespace Image_Compressor
 
 		public void DrawRepresentation(Image<Rgb24> image, Rectangle rectangle)
 		{
-			LinearGradientBrush gradientBrush = new(new PointF(rectangle.Left, rectangle.Top), new PointF(rectangle.Right, rectangle.Bottom), GradientRepetitionMode.DontFill, new ColorStop(0f, new Color(aColor)), new ColorStop(1f, new Color(bColor)));
+			PointF firstPoint;
+			PointF secondPoint;
+
+			switch (sampleLine)
+			{
+				case SampleLine.TopLeftToBottomRight:
+					firstPoint = new(rectangle.Left, rectangle.Top);
+					secondPoint = new(rectangle.Right, rectangle.Bottom);
+					break;
+				case SampleLine.TopRightToBottomLeft:
+					firstPoint = new(rectangle.Right, rectangle.Top);
+					secondPoint = new(rectangle.Left, rectangle.Bottom);
+					break;
+				case SampleLine.TopCenterToBottomCenter:
+					firstPoint = new(rectangle.Left + (rectangle.Width / 2), rectangle.Top);
+					secondPoint = new(rectangle.Left + (rectangle.Width / 2), rectangle.Bottom);
+					break;
+				case SampleLine.RightCenterToLeftCenter:
+					firstPoint = new(rectangle.Right, rectangle.Top + (rectangle.Height / 2));
+					secondPoint = new(rectangle.Left, rectangle.Top + (rectangle.Height / 2));
+					break;
+
+				default:
+					goto case SampleLine.TopLeftToBottomRight;
+			}
+
+			LinearGradientBrush gradientBrush = new(firstPoint, secondPoint, GradientRepetitionMode.DontFill, new ColorStop(0f, new Color(aColor)), new ColorStop(1f, new Color(bColor)));
 
 			image.Mutate(i => i.Fill(gradientBrush, new RectangularPolygon(rectangle)));
 
@@ -40,19 +69,26 @@ namespace Image_Compressor
 		{
 			var line = image.GetOptimalLinearGradientLine(rectangle);
 
+			LinearGradientFragment fragment;
+
 			switch (line)
 			{
-				case ColorUtils.SampleLine.TopLeftToBottomRight:
+				case SampleLine.TopLeftToBottomRight:
+					fragment = new(image[rectangle.Left, rectangle.Top], image[rectangle.Right - 1, rectangle.Bottom - 1], line);
 					break;
-				case ColorUtils.SampleLine.TopRightToBottomLeft:
+				case SampleLine.TopRightToBottomLeft:
+					fragment = new(image[rectangle.Right - 1, rectangle.Top], image[rectangle.Left, rectangle.Bottom - 1], line);
 					break;
-				case ColorUtils.SampleLine.TopCenterToBottomCenter:
+				case SampleLine.TopCenterToBottomCenter:
+					fragment = new(image[rectangle.Left + (rectangle.Width / 2), rectangle.Top], image[rectangle.Left + (rectangle.Width / 2), rectangle.Bottom - 1], line);
 					break;
-				case ColorUtils.SampleLine.RightCenterToLeftCenter:
+				case SampleLine.RightCenterToLeftCenter:
+					fragment = new(image[rectangle.Right, rectangle.Top + (rectangle.Height / 2)], image[rectangle.Left, rectangle.Top + (rectangle.Height / 2)], line);
 					break;
-			}
 
-			LinearGradientFragment fragment = new(image[rectangle.Left, rectangle.Top], image[rectangle.Right - 1, rectangle.Bottom - 1]);
+				default:
+					goto case SampleLine.TopLeftToBottomRight;
+			}
 
 			return fragment;
 		}
@@ -61,12 +97,14 @@ namespace Image_Compressor
 		{
 			binaryWriter.WriteColor(aColor);
 			binaryWriter.WriteColor(bColor);
+			binaryWriter.WriteByte((FragmentId)sampleLine, (byte)SampleLine._BitSize);
 		}
 
 		public Fragment ReadSpecificFragmentData(BitBinaryReader binaryReader)
 		{
 			aColor = binaryReader.ReadColor();
 			bColor = binaryReader.ReadColor();
+			sampleLine = (SampleLine)binaryReader.ReadByte((byte)SampleLine._BitSize);
 
 			return this;
 		}
